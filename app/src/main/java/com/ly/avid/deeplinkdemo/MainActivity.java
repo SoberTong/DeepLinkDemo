1b6e832fae9b7b58b15e09c4dbbc875218762b65
Sober <15821589161@163.com>
2017-02-07 15:07:05 +0800

    Initial commit

---
+++ app/src/main/java/com/ly/avid/deeplinkdemo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ly.avid.deeplinkdemo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txt = (TextView) findViewById(R.id.txt);
+
+        Intent it = getIntent();
+        if (it.getData() != null){
+            Uri uri = it.getData();
+            String scheme = uri.getScheme();
+            String host = uri.getHost();
+            String id = uri.getQueryParameter("id");
+            String name = uri.getQueryParameter("name");
+            String path = "scheme:"+scheme+", host:"+host+", id:"+id+", name:"+name;
+            Log.e("DeepLinkDemo", path);
+            Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+            txt.setText(path);
+        }
+    }
+}
